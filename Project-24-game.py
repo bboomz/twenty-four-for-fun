b432bdb69787b50414c9696c423298e05cc027ca
s7070077 <s7070077@kmitl.ac.th>
2014-12-09 22:04:19 +0700

    Update Project-24-game.py and make Interface

--- Project-24-game.py
+++ Project-24-game.py
@@ -1,4 +1,7 @@
-'''Game_24'''
+'''Project  MARK-II'''
+import sys
+from Tkinter import *
+import tkMessageBox
 lv1 = [['1', '2', '3', '4'], ['4', '4', '4', '4'], ['2', '2', '3', '3'], ['5', '5', '5', '5'], ['7', '6', '3', '6']]
 lv2 = [['2', '1', '7', '7'], ['1', '1', '8', '1'], ['2', '7', '7', '1'], ['3', '6', '1', '4'], ['5', '6', '4', '1']]
 lv3 = [['3', '3', '6', '6'], ['3', '3', '5', '5'], ['3', '3', '7', '7'], ['3', '3', '8', '8'], ['3', '3', '3', '3']]
@@ -11,78 +14,125 @@ lv9 = [['1', '7', '9', '2'], ['9', '8', '6', '2'], ['5', '5', '5', '1'], ['1', '
 lv10 = [['9', '8', '5', '4'], ['6', '3', '1', '3'], ['6', '7', '4', '3'], ['8', '8', '8', '3'], ['7', '5', '2', '4']]
 lis = [lv1, lv2, lv3, lv4, lv5, lv6, lv7, lv8, lv9, lv10]
 check = ['+', '-', '*', '/', '(', ')']
-def menu():
-    print 'GAME 24-FOR-FUN'
-    print 'PRESS 1 Play'
-    print 'PRESS 2 How to play'
-    choice = input()
-    if choice == 1:
-        game()
-    elif choice == 2:
-        howtoplay()
-    else:
-        menu()
-def howtoplay():
-    print 'Use 4 number and operator(+,-,*,/) to calculate it to 24'
-    print 'Press any key to Menu'
-    print 'Press 1 Play'
-    choice = input()
-    if choice == 1:
-        game()
-    else:
-        menu()
-def game():
-    lv_count = 1
-    score = 0
-    for i in lis:
-        for j in i:
-            print 'LV '+str(lv_count)
-            print j[0], j[1], j[2], j[3]
-            ans = calculate(raw_input(),j)
-            if ans == 24:
-                print 'Correct'
-                score += 1
-                print 'Your Score '+str(score)
-                print 'Next question'  
-                lv_count +=1
-            else:
-                print 'GAME OVER'
-                print 'Your answer is : '+str(ans)
-                print 'Your score : '+str(score)+' Point'
-                print 'Press any key to Menu'
-                print 'Press 1 How to play'
-                print 'Press 2 Try agin'
-                choice = input()
-                if choice == 1:
-                    howtoplay()
-                elif choice == 2:
-                    game()
-                else:
-                    menu()
-    print 'Congratulation'
-    print 'Yor Score is '+str(score)
-    print 'Thank you For Playing'
-    print 'Press any key to Menu'
-    menu()
-def calculate(ans, j):
-    lis_ans = []
-    count = 0
-    for k in ans:
-        if k not in j:
+class menuinterface(object):
+    '''main method'''
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry('450x450+200+200')
+        self.root.config(bg = 'black')
+        self.root.title('GAME-24-FOR-FUN')
+        #--Button and Label--
+        mlabel = Label(self.root,text = 'GAME-24-FOR-FUN', fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 45, y = 50)
+        self.button = Button(self.root, text = 'Play', command = lambda:self.root.destroy()&self.game24(0, 0, 0),bg = 'white', fg = 'black', font = ('purisa', 20)).place(x = 185, y = 200)
+        self.button = Button(self.root, text = 'Quit', command = self.quit24,bg = 'white', fg = 'black', font = ('purisa', 12)).place(x = 400, y = 400)
+        #--menubar--
+        menubar = Menu(self.root)
+        filemenu = Menu(menubar,tearoff = 0)
+        filemenu.add_command(label = 'Play', command = lambda:self.root.destroy()&self.game24(0, 0, 0))
+        menubar.add_cascade(label = 'Game',menu = filemenu)
+        helpmenu = Menu(menubar,tearoff = 0)
+        helpmenu.add_command(label = 'About us', command = self.aboutus)
+        helpmenu.add_command(label = 'How to play', command = self.howtoplay)
+        helpmenu.add_command(label = 'Credit',command = self.credit)
+        menubar.add_cascade(label = 'Help',menu = helpmenu)
+        self.root.config(menu = menubar)
+        self.root.mainloop()
+    def game24(self, j, lv,lv_game):
+        self.game = Tk()
+        self.game.geometry('450x450+200+200')
+        self.game.config(bg = 'black')
+        self.game.title('GAME-24-FOR-FUN')
+        #--menubar--
+        menubar = Menu(self.game)
+        filemenu = Menu(menubar,tearoff = 0)
+        filemenu.add_command(label = 'Menu', command = lambda:self.game.destroy()&self.__init__())
+        menubar.add_cascade(label = 'Game',menu = filemenu)
+        helpmenu = Menu(menubar,tearoff = 0)
+        helpmenu.add_command(label = 'About us', command = self.aboutus)
+        helpmenu.add_command(label = 'How to play', command = self.howtoplay)
+        helpmenu.add_command(label = 'Credit',command = self.credit)
+        menubar.add_cascade(label = 'Help',menu = helpmenu)
+        display = StringVar()
+        mlabel = Label(self.game,text = 'LV.'+str(lv_game) , fg = 'white', font = ('purisa', 12),bg = 'black').place(x = 10, y = 10)
+        mlabel = Label(self.game,text = (lis[lv][j][0], lis[lv][j][1], lis[lv][j][2], lis[lv][j][3]) , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 155, y = 50)
+        gentry = Entry(self.game,textvariable = display).place(x = 160, y = 150)
+        gbutton = Button(self.game,text = 'OK', command = lambda:self.calculate(display.get(), j, lv,lv_game), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 195, y = 200)
+        self.game.config(menu = menubar)
+        self.game.mainloop()     
+    def quit24(self):
+        self.root.destroy()
+    #--MessageBox--
+    def howtoplay(self):
+        tkMessageBox.showinfo(title='How to play',message='How to play')
+        return
+    def aboutus(self):
+        tkMessageBox.showinfo(title='About us',message='This is project by freshy college IT@KMITL')
+        return
+    def credit(self):
+        tkMessageBox.showinfo(title='CREDIT',message='\n MR.Phossawat Pruekphanasant IT57070077 Sec2 \n MR.Apchai Tuntasiri IT57070142 Sec3')
+        return
+    def calculate(self,display, j, lv,lv_game):
+        if j == 5:
+            j = 0
+            lv += 1
+        ans = str(display)
+        lis_ans = []
+        for k in display:
+            if k not in lis[lv][j]:
+                if k not in check:
+                    self.cannot(j, lv, lv_game,k)
+            elif k in lis[lv][j]:
+                k = float(k)
+            lis_ans.append(k)
             if k not in check:
-                print 'Can not use '+k
-                print 'plese try again'
-                print j[0], j[1], j[2], j[3]
-                calculate(raw_input(), j)
-        elif count > 4:
-            print 'Can not use number more than 4'
-            print 'plese try again'
-            print j[0], j[1], j[2], j[3]
-            calculate(raw_input(), j)
-        elif k in j:
-            k = float(k)
-            count += 1
-        lis_ans.append(k)
-    ans = eval(str(ans))
-    return ans
-menu()
+                if lis_ans.count(k) > lis[lv][j].count(str(int(k))):
+                    self.counterror(j, lv, lv_game,k)
+        ans = eval(str(ans))
+        if ans == 24:
+            j += 1
+            lv_game += 1
+            self.win_f(j, lv, lv_game)
+        else:
+            self.lose(j, lv, lv_game)
+    def win_f(self, j, lv, lv_game):
+        self.game.destroy()
+        self.win = Tk()
+        self.win.geometry('450x450+200+200')
+        self.win.config(bg = 'black')
+        self.win.title('GAME-24-FOR-FUN')
+        mlabel = Label(self.win,text = 'CORRECT' , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 120, y = 50)
+        mlabel = Label(self.win,text = 'You LV.'+str(lv_game) , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 130, y = 100)
+        gbutton = Button(self.win,text = 'NEXT', command = lambda:self.win.destroy()&self.game24(j, lv,lv_game), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 195, y = 200)
+        self.win.mainloop()
+    def lose(self, j, lv, lv_game):
+        self.game.destroy()
+        self.lose = Tk()
+        self.lose.geometry('450x450+200+200')
+        self.lose.config(bg = 'black')
+        self.lose.title('GAME-24-FOR-FUN')
+        mlabel = Label(self.lose,text = 'GAME OVER' , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 110, y = 50)
+        mlabel = Label(self.lose,text = 'You LV.'+str(lv_game) , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 130, y = 100)
+        gbutton = Button(self.lose,text = 'Menu', command = lambda:self.lose.destroy()&self.__init__(), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 195, y = 200)
+        self.lose.mainloop()
+    def cannot(self, j, lv, lv_game,k):
+        self.game.destroy()
+        self.cannot_gui = Tk()
+        self.cannot_gui.geometry('450x450+200+200')
+        self.cannot_gui.config(bg = 'black')
+        self.cannot_gui.title('GAME-24-FOR-FUN')
+        mlabel = Label(self.cannot_gui,text = 'ERROR' , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 140, y = 50)
+        mlabel = Label(self.cannot_gui,text = 'Cannot use '+str(k) , fg = 'white', font = ('purisa', 20),bg = 'black').place(x = 120, y = 100)
+        gbutton = Button(self.cannot_gui,text = 'Try Again', command = lambda:self.cannot_gui.destroy()&self.game24(j, lv, lv_game), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 195, y = 200)
+        self.cannot_gui.mainloop()
+    def counterror(self, j, lv, lv_game,k):
+        self.game.destroy()
+        self.counterror_gui = Tk()
+        self.counterror_gui.geometry('450x450+200+200')
+        self.counterror_gui.config(bg = 'black')
+        self.counterror_gui.title('GAME-24-FOR-FUN')
+        mlabel = Label(self.counterror_gui,text = 'ERROR' , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 140, y = 50)
+        mlabel = Label(self.counterror_gui,text = 'You use '+str(k)+' over the limit' , fg = 'white', font = ('purisa', 20),bg = 'black').place(x = 90, y = 100)
+        gbutton = Button(self.counterror_gui,text = 'Try Again', command = lambda:self.counterror_gui.destroy()&self.game24(j, lv, lv_game), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 195, y = 200)
+        self.counterror_gui.mainloop()
+menuinterface()
+
