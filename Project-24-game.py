8e4c838102a07a034fcf9248519c3ecbfe088f57
bboomz <s7070142@kmitl.ac.th>
2014-12-17 19:30:39 +0700

    debug
    debug

--- Project-24-game.py
+++ Project-24-game.py
@@ -62,7 +62,7 @@ class menuinterface(object):
         mlabel = Label(self.game,text = 'LV.'+str(lv_game) , fg = 'white', font = ('purisa', 12),bg = 'black').place(x = 10, y = 10)
         mlabel = Label(self.game,text = (lis[lv][j][0], lis[lv][j][1], lis[lv][j][2], lis[lv][j][3]) , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 155, y = 50)
         gentry = Entry(self.game,textvariable = display).place(x = 160, y = 150)
-        gbutton = Button(self.game,text = 'OK', command = lambda:self.calculate(display.get(), j, lv,lv_game), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 195, y = 200)
+        gbutton = Button(self.game,text = 'OK', command = lambda:self.calculate(display.get(), j, lv,lv_game), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 205, y = 200)
         self.game.config(menu = menubar)
         self.game.mainloop()     
     def quit24(self):
@@ -75,7 +75,11 @@ class menuinterface(object):
         '''
         how to play message
         '''
-        tkMessageBox.showinfo(title='How to play',message='How to play')
+        tkMessageBox.showinfo(title='How to play',message='1.There are only four numbers.\n'
++                              '2.Think of the factors of 24: 1x24, 2x12, 3x8, and 4x6. Try to make factors if it is what you are can do with your numbers.\n'
++                              '3.Make factors with the four numbers shown in the first step.\n'
++                              '4.Solve the problem. 2x4=8, now when you have used 2 and 4, you may not use them again. \n'
++                              'Now multiply your numbers together. 8x3=24. You may not repeat ANY numbers. You may only use each number once.\n')
         return
     def aboutus(self):
         '''
@@ -87,7 +91,7 @@ class menuinterface(object):
         '''
         credit message
         '''
-        tkMessageBox.showinfo(title='CREDIT',message='\n MR.Phossawat Pruekphanasant IT57070077 Sec2 \n MR.Apchai Tuntasiri IT57070142 Sec3')
+        tkMessageBox.showinfo(title='CREDIT',message='\n MR.Phossawat Pruekphanasant IT57070077 Sec2 \n MR.Apchai Tuntasirin IT57070142 Sec3')
         return
     def calculate(self,display, j, lv,lv_game):
         '''
@@ -95,16 +99,20 @@ class menuinterface(object):
         '''
         ans = str(display)
         lis_ans = []
+        num_check = 0
         for k in display:
             if k not in lis[lv][j]:
                 if k not in check:
                     self.cannot(j, lv, lv_game,k)
             elif k in lis[lv][j]:
                 k = str(float(k))
+                num_check += 1
             lis_ans.append(k)
             if k not in check:
                 if lis_ans.count(k) > lis[lv][j].count(k[0]):
                     self.counterror(j, lv, lv_game,k)
+        if num_check != 4:
+            self.numerror(j, lv, lv_game,k)
         ans = eval(str(''.join(lis_ans)))
         if ans == 24:
             j += 1
@@ -112,7 +120,10 @@ class menuinterface(object):
             if j == 5:
                 j = 0
                 lv += 1
-            self.win_f(j, lv, lv_game)
+            if lv == 50:
+                self.win_game(j, lv, lv_game)
+            else:
+                self.win_f(j, lv, lv_game)
         else:
             self.lose(j, lv, lv_game)
     def win_f(self, j, lv, lv_game):
@@ -125,9 +136,22 @@ class menuinterface(object):
         self.win.config(bg = 'black')
         self.win.title('GAME-24-FOR-FUN')
         mlabel = Label(self.win,text = 'CORRECT' , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 120, y = 50)
-        mlabel = Label(self.win,text = 'You LV.'+str(lv_game) , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 130, y = 100)
+        mlabel = Label(self.win,text = 'You LV.'+str(lv_game) , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 130, y = 100) 
         gbutton = Button(self.win,text = 'NEXT', command = lambda:self.win.destroy()&self.game24(j, lv,lv_game), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 195, y = 200)
         self.win.mainloop()
+    def win_game(self, j, lv, lv_game):
+        '''
+        player clear the game
+        '''
+        self.game.destroy()
+        self.wing = Tk()
+        self.wing.geometry('450x450+200+200')
+        self.wing.config(bg = 'black')
+        self.wing.title('GAME-24-FOR-FUN')
+        mlabel = Label(self.wing,text = 'YOU WIN' , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 115, y = 50)
+        mlabel = Label(self.wing,text = 'You LV.'+str(lv_game) , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 130, y = 100)
+        gbutton = Button(self.wing,text = 'Menu', command = lambda:self.wing.destroy()&self.__init__(), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 195, y = 200)
+        self.wing.mainloop()
     def lose(self, j, lv, lv_game):
         '''
         game lose display
@@ -167,5 +191,17 @@ class menuinterface(object):
         mlabel = Label(self.counterror_gui,text = 'You use '+str(k)+' over the limit' , fg = 'white', font = ('purisa', 20),bg = 'black').place(x = 90, y = 100)
         gbutton = Button(self.counterror_gui,text = 'Try Again', command = lambda:self.counterror_gui.destroy()&self.game24(j, lv, lv_game), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 195, y = 200)
         self.counterror_gui.mainloop()
+    def numerror(self, j, lv, lv_game,k):
+        '''
+        error player use number less than 4
+        '''
+        self.game.destroy()
+        self.num = Tk()
+        self.num.geometry('450x450+200+200')
+        self.num.config(bg = 'black')
+        self.num.title('GAME-24-FOR-FUN')
+        mlabel = Label(self.num,text = 'ERROR' , fg = 'white', font = ('purisa', 30),bg = 'black').place(x = 140, y = 50)
+        mlabel = Label(self.num,text = 'You use incomplete number' , fg = 'white', font = ('purisa', 20),bg = 'black').place(x = 70, y = 100)
+        gbutton = Button(self.num,text = 'Try Again', command = lambda:self.num.destroy()&self.game24(j, lv, lv_game), font = ('purisa', 10), bg = 'white', fg = 'black').place(x = 195, y = 200)
+        self.num.mainloop()
 menuinterface()
-
